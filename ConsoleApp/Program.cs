8a6f548a2543ff693989a9cbc6e5ebd2e871fea6
timurshalaev <timurshalaev2008@gmail.com>
2024-11-01 20:47:53 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int num_1=int.Parse(Console.ReadLine());
+            int num_2=int.Parse(Console.ReadLine());
+            int sum=num_1+num_2;
+            Console.WriteLine(sum);
+
+            Console.Write("side=");
+            int side=Convert.ToInt32(Console.ReadLine());
+            int perimetr = 4 * side;
+            Console.WriteLine($"perimetr={perimetr}");
+
+            Console.Write("R=");
+            int radius=int.Parse(Console.ReadLine());
+            double area=Math.PI*Math.Pow(radius, 2);
+            Console.WriteLine($"S={area:f2}");
+            Console.ReadLine();
+        }
+    }
+}
